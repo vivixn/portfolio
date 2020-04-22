d2ade8f1b8b16e5760dd2ff9455b5e59ee0b7129
vivixn <linv927@newschool.edu>
2020-04-22 00:58:32 -0400

    updated text size and color

--- page2/main.js
+++ page2/main.js
@@ -1,61 +1,61 @@
 var points = [
   {
-    "name": "<a href='images/pace.png'>pace gallery guide</a>",
+    "name": "&nbsp&nbsp <a href='images/pace.png'>pace gallery guide</a>",
     "emoji":"🎨",
     "pic":"<a href='images/pace.png'><img src='images/1.png' width='100%' height='100%'></a>",
   },
   {
-    "name":"<a href='images/sweetgreenvis.jpeg'>anatomy of sweetgreen</a>",
+    "name":"&nbsp&nbsp <a href='images/sweetgreenvis.jpeg'>anatomy of sweetgreen</a>",
     "emoji":"🥗",
     "pic":"<a href='images/sweetgreenvis.jpeg'><img src='images/4.png' width='100%' height='100%'></a>",
   },
   {
-    "name":"<a href='images/Momabrochure.pdf'>moma handles brochure</a>",
+    "name":"&nbsp&nbsp <a href='images/Momabrochure.pdf'>moma handles brochure</a>",
     "emoji":"🖼️",
     "pic":"<a href='images/Momabrochure.pdf'><img src='images/5.png' width='100%' height='100%'></a>",
   },
   {
-    "name":"<a href='images/bakebook.pdf'>typeset recipe book</a>",
+    "name":"&nbsp&nbsp <a href='images/bakebook.pdf'>typeset recipe book</a>",
     "emoji":"🍰",
     "pic":"<a href='images/bakebook.pdf'><img src='images/3.png' width='100%' height='100%'></a>",
   },
   {
-    "name":"<a href='images/tie.jpeg'>how to tie a tie</a>",
+    "name":"&nbsp&nbsp <a href='images/tie.jpeg'>how to tie a tie</a>",
     "emoji":"👔",
     "pic":"<a href='images/tie.jpeg'><img src='images/10.png' width='100%' height='100%'></a>",
   },
   {
-    "name":"<a href='images/debatefinal.pdf'>wim crouwel vs. van toorn</a>",
+    "name":"&nbsp&nbsp <a href='images/debatefinal.pdf'>wim crouwel vs. van toorn</a>",
     "emoji":"🤼",
     "pic":"<a href='images/debatefinal.pdf'><img src='images/8.png' width='100%' height='100%'></a>",
   },
   {
-    "name":"<a href='images/colorfilm.pdf'>color 35mm</a>",
+    "name":"&nbsp&nbsp <a href='images/colorfilm.pdf'>color 35mm</a>",
     "emoji":"🍭",
     "pic":"<a href='images/colorfilm.pdf'><img src='images/6.png' width='100%' height='100%'></a>",
   },
   {
-    "name":"<a href='images/bwfilm.pdf'>b/w 35mm</a>",
+    "name":"&nbsp&nbsp <a href='images/bwfilm.pdf'>b/w 35mm</a>",
     "emoji":"🎞️",
     "pic":"<a href='images/bwfilm.pdf'><img src='images/7.png' width='100%' height='100%'></a>",
   },
   {
-    "name":"<a href='images/digitalpics.pdf'>digital photography</a>",
+    "name":"&nbsp&nbsp <a href='images/digitalpics.pdf'>digital photography</a>",
     "emoji":"📷",
     "pic":"<a href='images/digitalpics.pdf'><img src='images/2.png' width='100%' height='100%'></a>",
   },
    {
-    "name":"<a href='images/vid2.mp4'>videography</a>",
+    "name":"&nbsp&nbsp <a href='images/vid2.mp4'>videography</a>",
     "emoji":"📹",
     "pic":"<a href='images/vid2.mp4'><img src='images/11.png' width='100%' height='100%'></a>",
   },
    {
-    "name":"<a href='images/wtc2.pdf'>wtc socials + oculus signage</a>",
+    "name":"&nbsp&nbsp <a href='images/wtc2.pdf'>wtc socials + oculus signage</a>",
     "emoji":"🍎",
     "pic":"<a href='images/wtc2.pdf'><img src='images/12.png' width='100%' height='100%'></a>",
   },
    {
-    "name":"<a href='images/wtc1.pdf'>wtc merchandise</a>",
+    "name":"&nbsp&nbsp <a href='images/wtc1.pdf'>wtc merchandise</a>",
     "emoji":"👕",
     "pic":"<a href='images/wtc1.pdf'><img src='images/9.png' width='100%' height='100%'></a>",
   },]
@@ -70,7 +70,7 @@ var points = [
       .css({'left': Math.floor(Math.random() * 90) + "%",
           'top': Math.floor(Math.random() * 90) + "%"})
       .hover(function(){
-        $(this).css({'opacity': .95 , 'backgroundColor': 'peach'})
+        $(this).css({'opacity': .95 })
         .html(
         points[i].pic
 
